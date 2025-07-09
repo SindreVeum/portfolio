8b454db75923a217eaf03bfff61e658066dcebfb
Sindre Veum <sindreveum@Sindres-MacBook-Air.local>
2025-07-09 10:27:29 +0200

    tpyescript fix

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,9 +1,12 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { 
+    browser: true, 
+    es2020: true 
+  },
   extends: [
     'eslint:recommended',
-    '@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
